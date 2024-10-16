d4bf89e108c7b92624b9fc6e74fd5a0481e465a3
Hamed Saadati <152387571+99hamed@users.noreply.github.com>
2024-10-16 21:08:35 +0330

    Update 1.py

--- DSA exercise/chapter1/1.py
+++ DSA exercise/chapter1/1.py
@@ -1,1 +1,10 @@
-this is a test
+def divide(a,b,counter=0):
+    if b==0:
+      raise Exception('division zero')  
+    elif a==0:
+       return counter
+    counter+=1
+    return divide(a-b,b,counter)
+    
+print(divide(15,5))
+
