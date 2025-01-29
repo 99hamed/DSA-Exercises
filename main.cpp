589e72e5f8f56df74f24df1f9ba5da0970116e32
Hamed Saadati <hamedsaadati1384@yahoo.com>
2025-01-29 20:36:52 +0330

    fixed some bugs

--- main.cpp
+++ main.cpp
@@ -1,104 +1,129 @@
 #include <iostream>
 #include "queue.h"
+#include <map>
 using namespace std;
 
-//struct tree_node {
-//    string word;
-//    int count;
-//    tree_node* left;
-//    tree_node* right;
-//    tree_node(string w="", int a=0) : word(w), count(a), left(nullptr), right(nullptr) {}
-//};
 
 class huffman_coding {
-    node* head;
+    node *head;
     string phrase;
-public:
-    huffman_coding(string w="") :phrase(w), head(nullptr) {}
 
-    node* in_list(string a) {
-        node* p = head;
-        while (p != nullptr) {
-            if (a == p->word) {
-                return p;
-            }
-            p = p->next;
+
+public:
+
+    huffman_coding(string w = "") : phrase(w), head(nullptr) {}
+
+    node *in_list(string a) {
+
+        node *temp = head;
+        while (temp != nullptr) {
+            if (temp->word == a)
+                return temp;
+            temp = temp->next;
         }
+
         return nullptr;
     }
 
-    node* getter() {
-        return head;
-    }
 
     void make_table() {
-        int size = phrase.size();
-        for (int i = 0; i < size; i++) {
-            node* q = in_list(string(1, phrase[i]));
+
+        for (char c: phrase) {
+            node *q = in_list(string(1, c));
+
             if (q != nullptr) {
                 q->count++;
             } else {
-               node* p = new node(string(1, phrase[i]));
-                if (head == nullptr) {
-                    head = p;
-                } else {
-                    node* temp = head;
-                    while (temp->next != nullptr) {
-                        temp = temp->next;
-                    }
-                    temp->next = p;
-                }
+                node *p = new node(string(1, c), 1);
+                p->next = head;
+                head = p;
             }
         }
     }
-    void huffman_tree(){
+
+
+    node * make_huffman_tree() {
         pr_queue q;
-        node* p = head;
-        node* root=nullptr;
+        node *p = head;
+        node *root = nullptr;
         while (p != nullptr) {
             q.enqueue(p);
             p = p->next;
         }
-        node* temp=0;
-        while (!q.isempty()){
-            node* a=q.dequeue();
-            node* b=q.dequeue();
-            if(b== nullptr){
-                root=a;
+        node *temp = 0;
+        while (!q.isempty()) {
+            node *a = q.dequeue();
+            if (q.isempty()) {
+                root = a;
                 break;
             }
-            root=new node(b->word+a->word,a->count+b->count);
+            node *b = q.dequeue();
 
-            root->left=a;
-            root->right=b;
+            root = new node(b->word + a->word, a->count + b->count);
+
+            root->left = a;
+            root->right = b;
 
             q.enqueue(root);
-
         }
 
-        head = root;
 
-        // چاپ مقادیر
-        cout << "Root: " << head->word << "          " << head->count << endl;
-
-        if (head->left != nullptr)
-            cout << "Left: " << head->left->word << "          " << head->left->count << endl;
-
-        if (head->right != nullptr)
-            cout << "Right: " << head->right->word << "          " << head->right->count << endl;
-
+        return root;
 
     }
-};
 
-int main() {
+
+    void generateCodes(node *root, string code) {
+        if (!root) return;
+
+
+        if (!root->left && !root->right) {
+            root->code = code;
+        }
+
+        generateCodes(root->left, code + "0");
+        generateCodes(root->right, code + "1");
+    }
+
+    void printHuffmanCodes() {
+        cout << "Huffman Codes:\n";
+        node* p=head;
+
+        while(p!= nullptr){
+            cout<<p->word<<"    "<<p->code<<endl;
+            p=p->next;
+        }
+    }
+
+    node *getter() {
+        return head;
+    }
+
+
+
+};
+    int main() {
 //    string name = "aaaaagfaassddffa";
 
-    huffman_coding f("aaaaagfaassddffa");
-    f.make_table();
-    f.huffman_tree();
+        huffman_coding f("aaaaagfaassddffa");
+        f.make_table();
+        node *p = f.getter();
+        while (p != nullptr) {
+            cout << p->word << "    " << p->count << endl;
+            p = p->next;
+        }
 
+        node *q = f.make_huffman_tree();
+        f.generateCodes(q,"");
+        cout << "Root: " << q->word << "          " << q->count << endl;
 
-    return 0;
-}
+        if (q->left != nullptr)
+            cout << "Left: " << q->left->word << "          " << q->left->count << endl;
+
+        if (q->right != nullptr)
+            cout << "Right: " << q->right->word << "          " << q->right->count << endl;
+        f.printHuffmanCodes();
+
+        return 0;
+    }
 
